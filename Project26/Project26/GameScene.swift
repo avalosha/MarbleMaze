a0b9f6c2950ae5de4f715b80e403286a2cdb1e56
Álvaro Ávalos Hernández <avalosha0805@gmail.com>
2020-10-20 21:39:05 -0500

    Load first level.

--- Project26/Project26/GameScene.swift
+++ Project26/Project26/GameScene.swift
@@ -10,6 +10,55 @@ import SpriteKit
 class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
-        
+        loadLevel()
     }
+    
+    ///La propiedad categoryBitMask es un número que define el tipo de objeto para considerar colisiones.
+    ///La propiedad collisionBitMask es un número que define con qué categorías de objeto debe colisionar este nodo,
+    ///La propiedad contactTestBitMask es un número que define las colisiones sobre las que queremos ser notificados.
+    
+    func loadLevel() {
+        guard let levelURL = Bundle.main.url(forResource: "level1", withExtension: "txt") else {
+            fatalError("Could not find level1.txt in the app bundle.")
+        }
+        guard let levelString = try? String(contentsOf: levelURL) else {
+            fatalError("Could not load level1.txt from the app bundle.")
+        }
+
+        let lines = levelString.components(separatedBy: "\n")
+        let spriteNodes = SpriteNodes()
+        var node = SKSpriteNode()
+        node = spriteNodes.createBackground()
+        addChild(node)
+
+        for (row, line) in lines.reversed().enumerated() {
+            for (column, letter) in line.enumerated() {
+                let position = CGPoint(x: (64 * column) + 32, y: (64 * row) + 32)
+                
+                if letter == "x" {
+                    // load wall
+                    node = spriteNodes.createWall(in: position)
+                    addChild(node)
+                } else if letter == "v"  {
+                    // load vortex
+                    node = spriteNodes.createVortex(in: position)
+                    addChild(node)
+                } else if letter == "s"  {
+                    // load star
+                    node = spriteNodes.createStar(in: position)
+                    addChild(node)
+                } else if letter == "f"  {
+                    // load finish
+                    node = spriteNodes.createFlag(in: position)
+                    addChild(node)
+                } else if letter == " " {
+                    // this is an empty space – do nothing!
+                } else {
+                    fatalError("Unknown level letter: \(letter)")
+                }
+            }
+        }
+    }
+    
+    
 }
---
+++ Project26/Project26/SpriteNodes.swift
@@ -0,0 +1,82 @@
+//
+//  SpriteNodes.swift
+//  Project26
+//
+//  Created by Álvaro Ávalos Hernández on 20/10/20.
+//
+
+import SpriteKit
+
+enum CollisionTypes: UInt32 {
+    case player = 1
+    case wall = 2
+    case star = 4
+    case vortex = 8
+    case finish = 16
+}
+
+class SpriteNodes {
+    
+    func createWall(in position: CGPoint) -> SKSpriteNode {
+        let node = SKSpriteNode(imageNamed: "block")
+        node.position = position
+
+        node.physicsBody = SKPhysicsBody(rectangleOf: node.size)
+        node.physicsBody?.categoryBitMask = CollisionTypes.wall.rawValue
+        node.physicsBody?.isDynamic = false
+        
+        return node
+    }
+    
+    func createVortex(in position: CGPoint) -> SKSpriteNode {
+        let node = SKSpriteNode(imageNamed: "vortex")
+        node.name = "vortex"
+        node.position = position
+        node.run(SKAction.repeatForever(SKAction.rotate(byAngle: .pi, duration: 1)))
+        node.physicsBody = SKPhysicsBody(circleOfRadius: node.size.width / 2)
+        node.physicsBody?.isDynamic = false
+
+        node.physicsBody?.categoryBitMask = CollisionTypes.vortex.rawValue
+        node.physicsBody?.contactTestBitMask = CollisionTypes.player.rawValue
+        node.physicsBody?.collisionBitMask = 0
+        
+        return node
+    }
+    
+    func createStar(in position: CGPoint) -> SKSpriteNode {
+        let node = SKSpriteNode(imageNamed: "star")
+        node.name = "star"
+        node.physicsBody = SKPhysicsBody(circleOfRadius: node.size.width / 2)
+        node.physicsBody?.isDynamic = false
+
+        node.physicsBody?.categoryBitMask = CollisionTypes.star.rawValue
+        node.physicsBody?.contactTestBitMask = CollisionTypes.player.rawValue
+        node.physicsBody?.collisionBitMask = 0
+        node.position = position
+        
+        return node
+    }
+    
+    func createFlag(in position: CGPoint) -> SKSpriteNode {
+        let node = SKSpriteNode(imageNamed: "finish")
+        node.name = "finish"
+        node.physicsBody = SKPhysicsBody(circleOfRadius: node.size.width / 2)
+        node.physicsBody?.isDynamic = false
+
+        node.physicsBody?.categoryBitMask = CollisionTypes.finish.rawValue
+        node.physicsBody?.contactTestBitMask = CollisionTypes.player.rawValue
+        node.physicsBody?.collisionBitMask = 0
+        node.position = position
+        
+        return node
+    }
+    
+    func createBackground() -> SKSpriteNode {
+        let background = SKSpriteNode(imageNamed: "background.jpg")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        
+        return background
+    }
+}
